0486bc4d0844658333be5eca0f0519e6a5d13ae4
fadi <fadi.botros@grapesnberries.com>
2018-03-13 15:59:36 +0200

    Corrected the negative tests

--- NonreusableLoginViewModelTests/UserLoginStateTests.swift
+++ NonreusableLoginViewModelTests/UserLoginStateTests.swift
@@ -16,8 +16,8 @@ class UserLoginStateTests: XCTestCase {
         XCTAssertEqual(userLoginState.isLoggedIn, true)
         XCTAssertEqual(userLoginState.user, User(userName: "user1", email: "user1@users.com", phone: "123456789"))
         XCTAssertNotEqual(userLoginState.user, User(userName: "user2", email: "user1@users.com", phone: "123456789"))
-        XCTAssertNotEqual(userLoginState.user, User(userName: "user2", email: "user2@users.com", phone: "123456789"))
-        XCTAssertNotEqual(userLoginState.user, User(userName: "user2", email: "user2@users.com", phone: "123456781"))
+        XCTAssertNotEqual(userLoginState.user, User(userName: "user1", email: "user2@users.com", phone: "123456789"))
+        XCTAssertNotEqual(userLoginState.user, User(userName: "user1", email: "user1@users.com", phone: "123456781"))
         XCTAssertNil(userLoginState.error)
     }
     
